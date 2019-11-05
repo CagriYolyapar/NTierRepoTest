b89f3e9a45c769812d8d42ad186771f60fd2673e
Gökhan Yumurtacı <gyumurtac@hotmail.com>
2019-11-05 13:41:17 +0300

    DAL oluşturuldu.

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext() : base("MyConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
