5e8a49494a5d93639862fedd6a23d59f24a4d205
ozangaziarslan190796@outlook.com <ozangaziarslan190796@outlook.com>
2019-11-05 13:54:09 +0300

    IRepository ve BaseRepository açıldı.

---
+++ Project.BLL/DesignPattern/UnitOfWorks/BaseRep/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using Project.BLL.DesignPattern.UnitOfWorks.IntRep;
+using Project.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPattern.UnitOfWorks.BaseRep
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetPassives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetUpdateds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SpecialDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project.BLL/DesignPattern/UnitOfWorks/IntRep/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPattern.UnitOfWorks.IntRep
+{
+    public interface IRepository<T>
+    {
+        List<T> GetActive();
+        List<T> GetPassives();
+        List<T> GetUpdateds();
+        List<T> GetAll();
+
+
+
+        void Add(T item);
+        void Delete(T item);
+        void Update(T item);
+        void SpecialDelete(int id);
+
+
+
+        
+    }
+}
+   
